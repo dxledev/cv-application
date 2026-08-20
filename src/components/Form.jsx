ca17d1f103950344a413bdc344cf688725766da1
dxlefourtwenty <ddparmstrong2021@gmail.com>
2026-08-20 10:15:37 -0700

    added form files

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ function Section({ children, subheading, className }) {
         <h1 className="form-subheading">{subheading}</h1>
       </div> 
 
-      <div className={`grid text-2xl gap-5 ${className}`}>
+      <div className={`flex flex-col ${className}`}>
         {children} 
       </div> 
     </div>
@@ -14,85 +14,96 @@ function Section({ children, subheading, className }) {
 
 function GeneralInformation() {
   return (
-    <div className="grid grid-cols-3 text-2xl gap-5">
-      <input
-        placeholder="First Name"
-        className="input"
-      />
-      <input
-        placeholder="Middle Name"
-        className="input"
-      />
-      <input
-        placeholder="Last Name"
-        className="input"
-      />
-      <input 
-        placeholder="Email"
-        className="input"
-      />
-      <input 
-        placeholder="Phone Number"
-        className="input"
-      />
+    <div className="section">
+      <div className="grid grid-cols-1 lg:grid-cols-3 text-2xl gap-5">
+        <input
+          placeholder="First Name"
+          className="input"
+        />
+        <input
+          placeholder="Middle Name"
+          className="input"
+        />
+        <input
+          placeholder="Last Name"
+          className="input"
+        />
+        <input 
+          placeholder="Email"
+          className="input"
+        />
+        <input 
+          placeholder="Phone Number"
+          className="input"
+        />
+      </div>
+
+      <SubmitButton />
     </div>
-    
   );
 }
 
 function Education() {
   return (
-  <div className="grid grid-cols-3 text-2xl gap-5">
-    <input
-      placeholder="School"
-      className="input"
-    />
-    <input 
-      placeholder="Major"
-      className="input"
-    />
-    <input
-      placeholder="Field of Study"
-      className="input"
-    />
-    <input 
-      placeholder="Start Date"
-      className="input"
-    />
-    <input
-      placeholder="End Date"
-      className="input"
-    />
-  </div>
+    <div className="section">
+      <div className="grid grid-cols-1 lg:grid-cols-3 text-2xl gap-5">
+        <input
+          placeholder="School"
+          className="input"
+        />
+        <input 
+          placeholder="Major"
+          className="input"
+        />
+        <input
+          placeholder="Field of Study"
+          className="input"
+        />
+        <input 
+          placeholder="Start Date"
+          className="input"
+        />
+        <input
+          placeholder="End Date"
+          className="input"
+        />
+      </div>
+      <SubmitButton />
+    </div>
   );
 }
 
 function Experience() {
   return (
-    <div className="grid grid-cols-2 text-2xl gap-5">
-      <input 
-        placeholder="Company Name"
-        className="input"
-      />
-      <input 
-        placeholder="Position Title"
-        className="input"
-      />
-      <textarea 
-        placeholder="Responsibilities"
-        className="input overflow-hidden"
-      />
-      <div className="flex flex-row gap-5">
+    <div className="section">
+      <div className="grid grid-cols-1 lg:grid-cols-2 text-2xl gap-5">
         <input 
-          placeholder="Start Date"
-          className="input self-start"
+          placeholder="Company Name"
+          className="input"
         />
         <input 
-          placeholder="End Date"
-          className="input self-start"
+          placeholder="Position Title"
+          className="input"
         />
+        <textarea 
+          placeholder="Responsibilities"
+          className="input overflow-hidden"
+        />
+        <div className="flex flex-row gap-5 justify-between">
+          <input 
+            placeholder="Start Date"
+            className="input self-start w-1/2"
+          />
+          <input 
+            placeholder="End Date"
+            className="input self-start w-1/2"
+          />
+        </div>
       </div>
+
+      <SubmitButton />
     </div>
+    
   );
 }
 
@@ -118,8 +129,6 @@ function Form() {
       <Section subheading="Experience">
         <Experience />
       </Section>
-
-      <SubmitButton />
     </div>
   );
 }
